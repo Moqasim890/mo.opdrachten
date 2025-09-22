683dfbd5cb43b84ad60861cb0e619f74042d77e0
MO Mohammed <340452@student.mboutrecht.nl>
2025-09-22 09:35:18 +0200

    05  ( Style the Currently Active Navigation Link)

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -84,12 +84,9 @@
             <el-disclosure id="mobile-menu" hidden class="block md:hidden">
                 <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                     <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                    <a href="/" aria-current="page"
-                        class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white">home</a>
-                    <a href="/about"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                    <a href="/contact"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">contact</a>
+                <x-nav-link href="/">home</x-nav-link>
+                <x-nav-link href="/about">About</x-nav-link>
+                <x-nav-link href="/contact">contact</x-nav-link>
                 </div>
                 <div class="border-t border-white/10 pt-4 pb-3">
                     <div class="flex items-center px-5">
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,2 +1,3 @@
-<a {{$attributes}}>{{ $slot }}</a>
-
+<a href="/" class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}">
+   {{ $slot }}
+</a>
